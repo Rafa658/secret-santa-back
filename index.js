778698c9f0f20da402a79ade2c44283c29d3da5f
Rafael <rafollo@live.com>
2023-02-11 00:54:03 -0300

    Added first two http routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const connectToDatabase = require('./database')
+const res = require('express/lib/response')
 const port = 4000
 
 // basic configs for better running
@@ -13,6 +14,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json())
 
+app.listen(process.env.PORT || port, () => console.log(`Started at port ${port}`))
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 connectToDatabase()
 
-app.listen(process.env.PORT || port, () => console.log(`Started at port ${port}`))
+app.get('/', (req, res) => res.send("Running"))
+
+app.get('/get_person', async (req, res) => {
+    
+})
