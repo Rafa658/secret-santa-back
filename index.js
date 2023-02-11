920cdf511f6c2a3d21dc608e2c66e4e53f68b72a
Rafael <rafollo@live.com>
2023-02-11 09:57:30 -0300

    Added comments and update route

--- index.js
+++ index.js
@@ -15,16 +15,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json())
 
+// listen on env variable or port = 4000
 app.listen(process.env.PORT || port, () => console.log(`Started at port ${port}`))
 
+// cors configs
 app.use(function (_, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+// opening mongodb
 connectToDatabase()
 
+// backend routes
 app.get('/', (_, res) => res.send("Running"))
 
 app.get('/get_people', async (_, res) => {
@@ -56,6 +60,21 @@ app.delete('/delete_person', async (req, res) => {
     if (status) {
         res.status(200)
         res.send(`Deleted ${body.name}`)
+    } else {
+        res.status(400)
+        res.send('Error')
+    }
+})
+
+app.patch('/update_person', async (req, res) => {
+    var body = req.body
+
+    var status = PersonService.Update(body)
+
+    if (status) {
+        res.status(200)
+        if (body.name_upd) res.send(`Changed name to ${body.name_upd}`)
+        if (body.email_upd) res.send(`Changed email to ${body.email_upd}`)
     } else {
         res.status(400)
         res.send('Error')
--- services/PersonService.js
+++ services/PersonService.js
@@ -36,6 +36,15 @@ class PersonService {
             return false
         }
     }
-}
 
+    async asyncUpdate(body) {
+        if (body.name_upd && body.email_upd) return false
+
+        const filter = {"name": body.name, "email": body.email}
+        const update = {"name": body.name_upd, "email": body.email_upd}
+
+        await Person.findOneAndUpdate(filter, update, {new: true})
+        return true
+    }
+}
 module.exports = new PersonService()
