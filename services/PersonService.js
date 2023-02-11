e53988fc9c012cd1a761267df22a8f570d33d2e7
Rafael <rafollo@live.com>
2023-02-11 01:05:23 -0300

    Added PersonService and first function

---
+++ services/PersonService.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const person = require('../models/Person')
+
+const Person = mongoose.model('Person', person)
+
+class PersonSerice {
+    async Add(name, gender, pref) {
+        if (!name || !gender || !pref) return false
+
+        var newPerson = new Person ({name, gender, pref})
+
+        try {
+            await newPerson.save()
+            return true
+        } catch (error) {
+            return false
+        }
+    }
+}
